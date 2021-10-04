a38bdf4933445a6262a19a6de361f4742218f2b2
anditsung <anditsung@gmail.com>
2021-10-04 14:54:47 +0700

    javascript to es5 and websocket js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,8 +14,9 @@
 
         <!-- Scripts -->
         @routes
-        <script src="https://cdn.jsdelivr.net/npm/weakmap@0.0.6/weakmap.min.js"></script>
-        <script src="{{ mix('es6.support.js', 'vendor/web') }}" defer></script>
+{{--        <script src="https://cdn.jsdelivr.net/npm/weakmap-polyfill@2.0.4/weakmap-polyfill.min.js"></script>--}}
+{{--        <script src="https://cdn.jsdelivr.net/npm/symbol-es6@0.1.2/symbol-es6.js"></script>--}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.6/es6-sham.min.js" integrity="sha512-uOJK5irxcOtq1g09xXAQWrq5p/Z96ztb+spIVmPsgZk1GGbHrfkYL8wDkVerKHdHFjgL+uCDRfrb0j86x68yUQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="{{ mix('app.vanilla.js', 'vendor/web') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
